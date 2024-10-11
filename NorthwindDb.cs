20c301402fe290c38a258af7bd7d7f46c7d42b92
Minaaa1 <230393@astanait.edu.kz>
2024-10-11 13:22:04 +0500

    Add files via upload

---
+++ NorthwindDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.EntityModels;
+
+public class NorthwindDb : DbContext
+{
+    public DbSet<Customer> Customers { get; set; } = null!;
+
+    protected override void OnConfiguring(
+      DbContextOptionsBuilder optionsBuilder)
+    {
+        string database = "Northwind.db";
+        string path = Path.Combine(Environment.CurrentDirectory, database);
+        if (!File.Exists(path))
+        {
+            throw new FileNotFoundException($"Could not find database file: {path}");
+        }
+        string connection = $"Data Source={path}";
+        optionsBuilder.UseSqlite(connection);
+    }
+}
